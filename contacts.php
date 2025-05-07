2bbda371764e5b9a68579aef3d6cda8375e526e7
getech-team <orggetech@gmail.com>
2025-05-07 17:19:09 +0000

    Create contacts.php

---
+++ contacts.php
@@ -0,0 +1,14 @@
+<?php include 'header.html'; ?>
+
+<section class="contacts">
+    <h2>Nous contacter</h2>
+    <p><strong>Email :</strong> contact@getech.org</p>
+    <p><strong>Téléphone :</strong> +228 90 00 00 00</p>
+    <p><strong>Facebook :</strong> <a href="#">facebook.com/GeTech</a></p>
+    <p><strong>YouTube :</strong> <a href="#">youtube.com/@GeTech</a></p>
+    <p><strong>WhatsApp :</strong> <a href="#">wa.me/22890000000</a></p>
+    <p><strong>TikTok :</strong> <a href="#">tiktok.com/@GeTech</a></p>
+    <p><strong>X (Twitter) :</strong> <a href="#">x.com/GeTech</a></p>
+</section>
+
+<?php include 'footer.html'; ?>
