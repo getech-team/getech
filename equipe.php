afc578b2fbe03ddad348f95d71d360149b251de1
getech-team <orggetech@gmail.com>
2025-05-07 17:18:24 +0000

    Create equipe.php

---
+++ equipe.php
@@ -0,0 +1,17 @@
+<?php include 'header.html'; ?>
+
+<section class="equipe">
+    <h2>Notre Équipe</h2>
+    <div class="membre">
+        <img src="images/membre1.jpg" alt="Membre 1">
+        <h3><a href="membre1.php">Nom Membre 1</a></h3>
+        <p>Spécialiste en développement Python, interface utilisateur et base de données.</p>
+    </div>
+    <div class="membre">
+        <img src="images/membre2.jpg" alt="Membre 2">
+        <h3><a href="membre2.php">Nom Membre 2</a></h3>
+        <p>Expert en électronique embarquée et intelligence artificielle.</p>
+    </div>
+</section>
+
+<?php include 'footer.html'; ?>
