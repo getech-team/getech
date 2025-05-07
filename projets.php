47a1bb7733a4e46b3d1d1c55d44a543ec2b64562
getech-team <orggetech@gmail.com>
2025-05-07 17:16:05 +0000

    Create projets.php

---
+++ projets.php
@@ -0,0 +1,17 @@
+<?php include 'header.html'; ?>
+
+<section class="projets">
+    <h2>Les projets de GeTech</h2>
+    <div class="projet-module">
+        <img src="images/projet1_mini.jpg" alt="Projet 1">
+        <h3><a href="projet1.php">Projet 1</a></h3>
+        <p>Description détaillée du projet 1...</p>
+    </div>
+    <div class="projet-module">
+        <img src="images/projet2_mini.jpg" alt="Projet 2">
+        <h3><a href="projet2.php">Projet 2</a></h3>
+        <p>Description détaillée du projet 2...</p>
+    </div>
+</section>
+
+<?php include 'footer.html'; ?>
