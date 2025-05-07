01c2a41475a9ef7ee3f364158f4b928eabb69f43
getech-team <orggetech@gmail.com>
2025-05-07 17:14:22 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include 'header.html'; ?>
+
+<section class="intro">
+    <h2>Bienvenue sur le site officiel de GeTech</h2>
+    <p>GeTech est une association de passionnés de génie logiciel, d'électronique et d'innovation technologique.</p>
+</section>
+
+<section class="domaines">
+    <h3>Nos domaines d'intervention</h3>
+    <ul>
+        <li>Développement de logiciels</li>
+        <li>Automatisation</li>
+        <li>Reconnaissance faciale</li>
+        <li>Gestion d'établissements</li>
+    </ul>
+</section>
+
+<section class="projets">
+    <h3>Nos projets</h3>
+    <div class="projet-module">
+        <img src="images/projet1.jpg" alt="Projet 1">
+        <h4><a href="projet1.php">Projet 1</a></h4>
+        <p>Brève description du projet 1...</p>
+    </div>
+    <div class="projet-module">
+        <img src="images/projet2.jpg" alt="Projet 2">
+        <h4><a href="projet2.php">Projet 2</a></h4>
+        <p>Brève description du projet 2...</p>
+    </div>
+    <!-- Ajouter plus de projets -->
+</section>
+
+<?php include 'footer.html'; ?>
